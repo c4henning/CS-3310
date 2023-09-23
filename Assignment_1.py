d688a4ab7bf5b1c1c42d6f43beb297904b21969c
Christian Henning <122561083+c4henning@users.noreply.github.com>
2023-09-23 12:07:01 -0400

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -4,32 +4,60 @@
 
 class Node(object):
     # constructor for Node class
-    def __init__(self, data: any):
+    def __init__(self, data: any = None):
         self.data = data
         self.next: Node | None = None  # next node in list
         self.prev: Node | None = None  # previous node in list
     
-    
-class LinkedList(object):
-    __head = None
-    __tail = None
-    __capacity = 0
-    __size = 0
 
+class LinkedList(object):
     # constructor for LinkedList class
     def __init__(self):
-        # code goes here
-        pass
+        self.__header = Node()
+        self.__trailer = Node()
+        self.__header.next = self.__trailer
+        self.__trailer.prev = self.__header
+        self.__size = 0
 
     # add item x to list at index i
-    def add(self, i, x):
-        # code goes here
-        pass
+    def add(self, i: int, x: any):
+        if i < 0 or i > self.__size:
+            raise IndexError("Index out of range")
+        new_node = Node(x)
+        next_node = self.get_node(i)
+        prev_node = next_node.prev
+
+        new_node.next = next_node
+        new_node.prev = prev_node
+
+        next_node.prev = new_node
+
+        prev_node.next = new_node
+
+        self.__size += 1
 
     # remove item at index i from the list
     def remove(self, i):
-        # code goes here (should return item from list or None if item is not in the list)
-        pass
+        target_node = self.get_node(i)
+        data = target_node.data
+
+        next_node = target_node.next
+        prev_node = target_node.prev
+        next_node.prev = prev_node
+        prev_node.next = next_node
+
+        del target_node
+        self.__size -= 1
+        return data
+
+    # find node at specified index
+    def get_node(self, i):
+        if i < 0 or i >= self.__size:
+            raise IndexError("Index out of range")
+        current = self.__header.next
+        for _ in range(i):
+            current = current.next
+        return current
 
 
 class Stack(object):
@@ -66,17 +94,20 @@ class Stack(object):
 
     # returns Boolean of whether stack is currently full
     def is_full(self) -> bool:
-        return self.__stackPointer + 1 == Stack.__maxSize
+        return self.__stackPointer + 1 >= Stack.__maxSize
 
     # clears the stack
     def clear(self):
-        # code goes here
-        pass
+        self.__stackPointer = -1
 
     # looks at the top item of the stack without removing it
     def peek(self):
-        # code goes here
-        pass
+        if self.is_empty():
+            raise IndexError("stack is empty")
+        else:
+            item = self.__stack[self.__stackPointer]
+            print(f"top item: {item}")
+            return item
 
 
 class StackParenthesesChecker(object):
@@ -94,8 +125,18 @@ class StackParenthesesChecker(object):
 
 
 if __name__ == "__main__":
-    inputParens = input('Input parenthesises: ')
-    Parens = Stack()
-    for ch in inputParens:
-        if ch == '(' or ch == ')':
-            Parens.push(ch)
+#     while True:
+#         inputParens = input('(Q)uit or Input parenthesis: ')
+#
+#         if inputParens.upper() != 'Q':
+#             Parens = LinkedList()
+#             for ch in inputParens:
+#                 if ch == '(' or ch == ')':
+#                     Parens.add(ch, 0)
+#         else:
+#             break
+
+    lst = LinkedList()
+    lst.add(0, 'cat')
+    lst.add(0, 'dog')
+    lst.add(0, 'cow')
